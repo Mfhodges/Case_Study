d68631900388eee92c1e11c17ca25f1dd730c0c5
Maddy <mfhodges@haverford.edu>
2018-04-30 11:26:53 -0400

    added txt file input

--- JSON_API/data_set_importer.py
+++ JSON_API/data_set_importer.py
@@ -6,27 +6,20 @@ Provide routines to import DPLA project data set into list of lists format.
 & do a query & filter by one of the 4 fields
 
 Fields wanted:
- 1. Search Term ??
+ 1. Search Term
  2. ID
  3. Ingest Date
  4. Data Provider
 
 PARAMETERS
- &fields=id,dataProvider,ingestDate
+ &fields=id,dataProvider,ingestDate # there is an issue with ingestDate and it does not work like this
  &sort_order= 'asc' or 'desc'
- # SORT ON BY ID?
+ # SORT ON BY ID
  &sort_by= 'id' or 'dataProvider' or 'ingestDate'
  &api_key
-Request only the title and id fields
-http://api.dp.la/dev/items?q=town&fields=aggregatedCHO.title,id
-
-
-https://github.com/dpla/platform/wiki/Code4Lib-API-Documentation
-
 
 things to consider:
 how to handle an error when reading a file ?
-API call limits?
 
 For each item, the parent object docs contains information about how that item was imported into the database: for example, ingestDate and other context about the record (see @context).
 Down in the sourceResource object, there’s information about the title and subject of the work. Here, the enumerated subject name: "Weasels" probably accounts for this being the first result.
@@ -38,23 +31,24 @@ import json
 import csv
 import urllib.request
 import argparse
+from operator import itemgetter
 
-#parser = argparse.ArgumentParser(description='DPLA api call')
 
+parser = argparse.ArgumentParser(description='DPLA api call')
 #make the below necessary
-#add filter on field and to make ascending or descending
 #type check!
+parser.add_argument('-f', required=True,action='store',help='filename of input')
+parser.add_argument('-o', action='store',help='filename of output')
+parser.add_argument('--resource',required=True,action='store',help='the resource_type',choices=['items','collections'])
+parser.add_argument('-s',action='store',required=True,help='the field to sort on', choices=['id','dataProvider','ingestDate'])
+parser.add_argument('--asc',action="store_true",help='sort by the specified field, smallest to largest')
+parser.add_argument('--desc',action="store_true",help='sort by the specified field, largest to smallest')
+args = vars(parser.parse_args())
 
-#parser.add_argument('-q', '--q',
-#                    help='the term to query  ___')
-#parser.add_argument('-resource',
-#                    help='the resource_type')
-
-#args = vars(parser.parse_args())
 # change these for other api calls
 base_URL = 'https://api.dp.la/v2/'
 api_key = 'bd42082e8bd7b7eedf534a0256da1b6d'
-fields = ['id','dataProvider']#,'ingestDate']
+fields = ['id','dataProvider','ingestDate']
 stringfields = ','.join(fields)
 
 class get_json:
@@ -62,22 +56,20 @@ class get_json:
         self.url = url
         self.key = '&api_key='+api_key  # i would like to make this individual to me
         ## need to be set ##
-        self.qterm = 'q=' # this could be a list?
+        self.qterm = ''
         self.resource =None
         ## not required ##
-        self.sort_by= '&sort_by=' #single field
-        self.sort_order ='&sort_order=' #asecending or descending
+        self.sort_by= ''         #'&sort_by=' #single field
+        self.sort_order = ''     #'&sort_order=' #asecending or descending
         ## assumed ##
         self.fields = '&fields=' + stringfields # hard-coded - not ideal
-
-        #self.parameters = make this a list of the parameters !
 
     # Method for setting query
     def set_query(self,query=None):
         if not query:
             raise NameError('Provide a term to query for the API!')
         else:
-            self.qterm+=query
+            self.qterm=query
 
     # Method for setting query
     def set_resource(self,resource=None):
@@ -88,53 +80,98 @@ class get_json:
 
     def set_sort(self, sort_by=None, sort_order='asc'):
         #ascending or descending ?
-
         if not sort_by:
-            raise NameError('Provide a field to seach by : search_term, ID, ingest_date, data_provider')
+            raise NameError('Provide a field to sort by : search_term, ID, ingest_date, data_provider')
         elif sort_by not in fields:
-            raise NameError('Provide a proper field to seach by : search_term, ID, ingest_date, data_provider')
+            raise NameError('Provide a proper field to sort by : search_term, ID, ingest_date, data_provider')
         else:
             #typecheck that its in the fields !
-            self.sort_by += sort_by
+            self.sort_by = sort_by
             if sort_order == 'desc':
-                self.sort_order +='desc'
+                self.sort_order ='desc'
             else:
-                self.sort_order +='asc'
+                self.sort_order ='asc'
 
     # Extract data from json api
-    # EDIT THIS TO BE ABLE TO QUERY
-    def get_data(self, write_to_csv=False):
-        # must have self.resource .qterm and key filled out.
-        if self.resource == None or self.qterm == 'q=':
+    def get_data(self):
+        if self.resource == None or self.qterm == 'q=': # must have self.resource .qterm and key filled out.
             raise( 'set a qterm and resource before you call the api')
-        else:
-            url = self.url + self.resource +'?'+ self.qterm + self.fields + self.sort_by + self.sort_order + self.key
-        #return url
+        else: #+ ' &fields='+self.fields +'&sort_by=' +self.sort_by + '&sort_order='+self.sort_order
+            url = self.url + self.resource +'?'+ 'q='+self.qterm + self.key
+            ## ideally url would = self.url + self.resource +'?'+ self.qterm  + self.fields + self.sort_by + self.sort_order+ self.key
+            ## however, i was having an issue with ingestDate as a field so i am pulling the full query and then
+        print(url)
         with urllib.request.urlopen(url) as url:
             data = json.loads(url.read().decode())
+        count = data['count'] # total number of items that match that query
+        #max number of objects returned per page is 500. if not specified 10 are given.
+        #url = self.url + self.resource +'?'+ 'q='+self.qterm +page_size=500 + self.key
+        if count > 500:
+            pages = count // 500
+            if count % 500 != 0 : pages +=1
+        else: #otherwise we have less than 500 objects so one page will suffice
+            pages = 1
+        print("pages",pages,"count",count)
+        final_data = []
+        for i in range(1,pages+1):# now pull for each page.
+            url = self.url + self.resource +'?'+ 'q='+self.qterm +'&page_size=500&page='+str(i) + self.key
+            print(url,"\n")
+            with urllib.request.urlopen(url) as url:
+                data = json.loads(url.read().decode())
+                dataset = data['docs']
+                keys = {'id','dataProvider','ingestDate'}
+                filtered_data= list(map(lambda x: {k:v for k, v in x.items() if k in keys}, dataset)) # reduces to the 3 fields we want
+                filtered_data = [dict(item, **{'query':self.qterm}) for item in filtered_data] # adds the query as a field
+                final_data += filtered_data
+        print("did we get all objects?: ", count==len(final_data)) # just to check we have everything
 
+        # sorting if specified - reverse=True is descending
+        if self.sort_by:
+            if self.sort_order == 'desc':
+                final_data = sorted(final_data, key=itemgetter(self.sort_by), reverse=True)
+            else: #defaults to ascending
+                final_data = sorted(final_data, key=itemgetter(self.sort_by))
+        return final_data
 
-        return data
+    def write_to_csv(self,data,file_name):
+        #file_name must be a string
+        if file_name == None:
+            file_name = 'results'
+        with open( file_name +'.csv', 'w', newline='') as csvfile:
+            header = data[0].keys()
+            writer = csv.DictWriter(csvfile, fieldnames=header)
+            writer.writeheader()
+            for i in range(0,len(data)):
+                writer.writerow(data[i])
+
 
 if __name__ == '__main__':
-
-    # add stuff here !
     # dpla = get_json(base_URL)
-    #print(args,"\n")
+    print(args,"\n") # example = {'f': 'sample_input.txt', 'o': 'results', 'resource': 'items', 's': 'id', 'asc': True, 'desc': False}
+    with open(args['f']) as f:
+        queries = f.read().splitlines()
     dpla = get_json(base_URL,api_key)
-    dpla.set_query('kittens')
-    dpla.set_resource('items')
-    dpla.set_sort('id','asc')
-    dataset = dpla.get_data()['docs']
-    print(dataset)
-    with open('results.csv', 'w', newline='') as csvfile:
-        header = dataset[0].keys()
-        writer = csv.DictWriter(csvfile, fieldnames=header)
-        writer.writeheader()
-        for i in range(0,len(dataset)):
-            meetup = dataset[i].values()
-            writer.writerow(dataset[i])
+    dpla.set_resource(args['resource'])
+    if args['desc'] == False:
+        dpla.set_sort(args['s'],'asc')
+    else:
+        dpla.set_sort(args['s'],'desc')
+    results = []
+    for q in queries:
+        dpla.set_query(q) # this will be varied
+        dataset = dpla.get_data()#['docs']
+        results += dataset
+    dpla.write_to_csv(results,args['o'])
 
 
-        # data = dpla.get_data()
-        # print (data[0]['text'])
+    ##### HARD CODED EXAMPLE #####
+    #queries = ['kittens','kitten','cat']
+    #dpla = get_json(base_URL,api_key)
+    #dpla.set_resource('items')
+    #dpla.set_sort('id','asc')
+    #results = []
+    #for q in queries:
+    #    dpla.set_query('kittens') # this will be varied
+    #    dataset = dpla.get_data()#['docs']
+    #    results += dataset
+    #dpla.write_to_csv(results,'kittens')
--- README.md
+++ README.md
@@ -1,21 +1,23 @@
 # Welcome
 
-**Task:** "Provide routines to import DPLA project data set into list of lists format by do a query with the api and filter by one of the 4 fields"
+**Task:** "Provide routines to import DPLA project data set into list of lists format by doing a query with the api and filter by one of the 4 fields"
 
+### How To Run
+The python script `data_set_importer.py` accomplishes the task above and the parameters are set as command line arguments. An example of a command is:   
+`$ python3 data_set_importer.py -f <input.txt> -o <output_file_name>  --resource <'items' or 'collections'> -s <field_to_sort_on> --asc`  
 
+The flags '-f' and '--resource' are required . '-f' specifies the the input (defined below) and '--resource' specifies the resource type (either items or collections).  '-o' is the name of the output file and is optional (defaults to 'results').'-s' followed by one of the fields specifies to sort on that field and is optional. The sort order is set by the flag '--asc' (ascending) or '--desc' (descending). The sort order is optional and defaults to ascending if a field to sort on is provided and a sort order is not.
 
-< Brief how to run goes here >  
-reminder it defaults to python3
 
 **Input:**
  "Your program should accept a file containing a list of item terms (one term per line) to search for via the DPLA API and should provide a way to specify which report field(s) to sort on."
 
 
 **Output:**
-Report in a format of your choosing (CSV, HTML, Excel,etc )
+A CSV ( name specified by '-o' flag ).
 
 
-
+## Building the API call
 Two Resource types:
  - collections
  - items
@@ -24,29 +26,30 @@ Fields wanted:
  1. Search Term  
  provided by user
  2. ID  `id`  
- DPLA ID of a SourceResource within a given context.
-
+ DPLA ID of a SourceResource within a given context.  
  3. Ingest Date  `ingestDate`  
  Date on which the original record was imported into the DPLA database.
- 4. Data Provider `provider`  
+ 4. Data Provider `Dataprovider`  
  Service or content hub providing access to the Data Provider’s content. May contain the same value as Data Provider. (literal value in this version) (EDM)
 
 Example:
 	https://api.dp.la/v2/items?q=kittens&api_key=XXXXX
 
-Semantic URL Units
- 1. base_URL: https://api.dp.la/v2/
- 2. Resource_type: items or collections ( path )  
-
- **( Parameters )**  
- 3. query_Indicator: '?q'  (FIND BETTER WORD FOR THIS )  
- 4. query_term: 'kittens'  
- 5. api_part: 'api_key'  
- 6. my_api_key: XXXXXXX     
+URL Units (in order)
+ 1. base_URL: `https://api.dp.la/v2/`
+ 2. Resource_type: 'items' or 'collections'
+ **( Parameters )**   
+ 4. query term: q=<term>
+ 5. fields: fields=id,dataProvider,ingestDate
+ 6. sort by: sort_by=<one of the fields>
+ 7. sortorder: sort_order=<asc or dec>
+ 8. api_key: api_key=<api_key>     
 
 Within a URL, parameters follow the '?'. They are specified by '<parameter>=<user_input>' are separated by '&'.
 
+## How my code works
+The above describes the ideal way to build an API query however, I had an issue with ingestDate so I do not specify any of 5.-7. from the Url Units and instead use python to filter and manipulate the response as desired.
 
 
-
-"""
+## Contributors :tada:
+:octocat: [Maddy Hodges](https://github.com/Mfhodges)
